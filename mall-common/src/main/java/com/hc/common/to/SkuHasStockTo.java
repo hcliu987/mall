20a5b8cf30541f11b930676dd09c76e1a23f0c27
liuhaicheng <hc.liu987@gmail.com>
2024-01-23 21:31:05 +0800

    nacos 配置修改

---
+++ mall-common/src/main/java/com/hc/common/to/SkuHasStockTo.java
@@ -0,0 +1,13 @@
+package com.hc.common.to;
+
+import lombok.Data;
+
+@Data
+public class SkuHasStockTo {
+    private Long skuId;
+    private boolean hasStock;
+
+    public boolean getHasStock() {
+        return this.hasStock;
+    }
+}
