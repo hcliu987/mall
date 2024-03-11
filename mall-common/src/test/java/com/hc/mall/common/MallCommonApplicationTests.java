b2f19215ee756234bcefd24adc9a7b05c8d8aadb
liuhaicheng <hc.liu987@gmail.com>
2024-03-11 16:46:10 +0800

    多个模块重构 2

---
+++ mall-common/src/test/java/com/hc/mall/common/MallCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hc.mall.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
